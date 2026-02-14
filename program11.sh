1d03f5df9db4aa367b3631b3608003dbbec5f639
Abisin Raj <abisinr201@gmail.com>
2026-02-14 09:33:25 +0530

    Add example output to program11

--- program11.sh
+++ program11.sh
@@ -35,3 +35,15 @@ Gross=$(echo "scale=2;$BP+($BP*$DA/100)+($BP*$HRA/100)" | bc)
 # Print the calculated Gross Salary for the employee
 # $name and $Gross are interpolated into the string
 echo "Gross salary of $name is Rs $Gross"
+
+# __________________________________________
+# | Enter the name of the employee         |
+# | John                                   |
+# | Enter the Basic Pay of the employee    |
+# | 10000                                  |
+# | Enter the DA of the employee           |
+# | 10                                     |
+# | Enter the HRA of the employee          |
+# | 20                                     |
+# | Gross salary of John is Rs 13000.00    |
+# |________________________________________|
